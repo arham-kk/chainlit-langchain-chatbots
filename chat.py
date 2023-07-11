dcfb2a19950ab3c066a37bcaed6fa60c488a2af0
Arham <108623726+arham-kk@users.noreply.github.com>
2023-07-11 18:02:14 +0500

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,14 @@
+import os
+from langchain import PromptTemplate, OpenAI, LLMChain
+import chainlit as cl
+
+os.environ["OPENAI_API_KEY"] = "OPENAI_API_KEY"
+
+template = """Question: {question} Answer: Let's think step by step."""
+
+@cl.langchain_factory(use_async=True)
+def factory():
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
+
+    return llm_chain
